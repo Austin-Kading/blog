461ded8232fb560c01b567ad89d6cc0ebde12a1a
Austin Kading <email": null>
2017-05-11 12:07:33 -0700

    adds posts migration

--- db/migrate/20170511120250_posts.rb
+++ db/migrate/20170511120250_posts.rb
@@ -1,4 +1,10 @@
 class Posts < ActiveRecord::Migration
   def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps null: false
+    end
   end
 end
